812cda2de7bc8635604b159cadbc24625e305594
chloeongo <chloe.ongolesono@gmail.com>
2024-04-10 14:00:40 +0200

    nieuwe opdrachten

---
+++ Find the Bugs/findTheBugs.js
@@ -0,0 +1,38 @@
+//Age-Calculator
+//line 32  displayAge(year, month, day);
+
+//Analog-clock
+//line 16 let secHandRotation = 6 * seconds;
+
+//BMI-Calculator
+//line 25 bmiElem.style.borderColor = "#b00";
+
+//Box-shadow-generator
+//line 19   box.style.boxShadow = `${hOffset}px ${vOffset}px ${blurRadius}px ${spread}px ${color}`;
+
+//Currency-Converter
+//(HTML) line 54 <option value="CAD">CAD</option>
+
+//Form-Validation
+//line 48 dealErrorFor(confirmPasswordElem, `Password didn't match`);
+
+//HTML-Canvas
+//line 42 window.addEventListener("mouseup", stopDrawing);
+
+//Password-Generator
+//line 62 const SYMBOL_CHAR_CODES = arrayLowToHigh(123, 126)
+
+//Quotes-Generator
+//line 14 authorElement.innerText = `―${data.author}`;
+
+//Random-Color-Generator
+//line 6 randomHex = randomHex.substring(randomHex.length - 6);
+
+//Superhero-card
+//line 41 durabilityEl.innerText = data.powerstats.durability;
+
+//Tip-Calculator
+//line 15 let persons = splitInputElem.value * 1;
+
+//Todo-list
+//(CSS) line 79 text-decoration: line-through;
